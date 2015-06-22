0e8befca4f150ce58683614662985cf0e32d4b47
ThachTranQuoc <quocthachah11@yahoo.com>
2015-06-22 13:36:18 +0700

    [ADD]Add new project intergrate Cocoapods

---
+++ onWorld/onWorld/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  OnWorld
+//
+//  Created by yestech1 on 6/22/15.
+//  Copyright (c) 2015 OnWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+
+
+@end
+
---
+++ onWorld/onWorldTests/onWorldTests.m
@@ -0,0 +1,40 @@
+//
+//  OnWorldTests.m
+//  OnWorldTests
+//
+//  Created by yestech1 on 6/22/15.
+//  Copyright (c) 2015 OnWorld. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface OnWorldTests : XCTestCase
+
+@end
+
+@implementation OnWorldTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
